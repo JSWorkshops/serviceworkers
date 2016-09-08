3b01c155bfd20119d922877af5118dc7460c5d16
Marcos Caceres <marcos@marcosc.com>
2016-09-08 17:15:56 +1000

    fix: get everything into initial state

--- js/registration.js
+++ js/registration.js
@@ -1,48 +1,5 @@
 "use strict";
 async.task(function*() {
-  if (!navigator.serviceWorker) {
-    return;
-  }
-  const reg = yield navigator.serviceWorker.register("sw.js");
-
-  if (!navigator.serviceWorker.controller) {
-    console.log("First time running...");
-    return;
-  }
-
-  if (reg.waiting) {
-    console.log("Update is in waiting!");
-    updateReady(reg.waiting);
-    return;
-  }
-
-  if (reg.installing) {
-    trackInstallation(reg.installing);
-    return;
-  }
-
-  if (reg.active) {
-    console.log("SW is installed and active!");
-  }
-
-  reg.addEventListener("updatefound", (ev) => {
-    console.log("Found an update!");
-    trackInstallation(reg.installing);
-  });
-
-  navigator.serviceWorker.addEventListener('controllerchange', () => {
-    console.log("Controller changed, so reloading");
-    window.location.reload();
-  });
-
-  function trackInstallation(sw) {
-    sw.addEventListener("statechange", (ev) => {
-      console.log("state change", sw.state);
-      if (sw.state === "installed") {
-        updateReady(sw);
-      }
-    });
-  }
 
   function updateReady(sw) {
     const toast = document.querySelector("#update-toast");
@@ -52,7 +9,7 @@ async.task(function*() {
     dismiss.onclick = () => toast.classList.remove("show");
     refresh.onclick = () => {
       toast.classList.remove("show");
-      sw.postMessage({ action: "skipWaiting" });
+      // Let SW know it can take over
     }
   }
 }).catch(err => console.log(err));
--- sw_final.js
+++ sw_final.js
@@ -1,100 +1,62 @@
-/* jshint worker: true */
 /*globals async, caches, siteFiles, Response, fetch */
 "use strict";
-
 // Let's start bringing in things we need
 importScripts("js/lib/async.js");
 importScripts("js/siteFiles.js");
 
 console.info("Hello, I'm the service worker!");
 
-const SITE_CACHE = "v1";
-
-// Set up Service Worker tasks
-const SWTasks = {
-  cacheSite: async(function*(siteFiles, cacheName) {
-    console.info("Trying to off-line site resources into:", cacheName);
-    try {
-      const cache = yield caches.open(cacheName);
-      yield cache.addAll(siteFiles);
-    } catch (err) {
-      throw err;
-    }
-    console.log("Success! done caching site files!");
-  }),
-  networkFirst(request, cacheName) {
-    return async.task(function*() {
-      let response;
-      try {
-        response = yield fetch(request);
-        if (!response.ok) {
-          throw new Error("Got response, but was not ok!", response.status);
-        }
-      } catch (err) {
-        console.warn("Useless response, going to cache:", err);
-        return yield this.respondFromCache(request, cacheName);
-      }
-      // Response was ok, so let's cache it!
-      try {
-        yield this.saveToCache(request, response, cacheName);
-      } catch (err) {
-        console.error("Oh crap! Wasn't able to write to cache!", err);
-      }
-      return response;
-    }, this);
-  },
-  deleteAllCaches: async(function*() {
-    const keys = yield caches.keys();
-    const promisesToDelete = keys.map(
-      key => caches.delete(key)
-    );
-    yield Promise.all(promisesToDelete);
-  }),
-  respondFromCache: async(function*(request, cacheName) {
-    const cache = yield caches.open(cacheName);
-    let response = yield cache.match(request);
-    // We have this, yay!
-    if (response) {
-      return response;
-    }
-    // Try to get it from the network and store it
-    try {
-      console.warn("Not in cache. Getting from network:", request.url || request);
-      const response = yield fetch(request);
-      yield cache.put(request, response.clone());
-    } catch (err) {
-      var msg = `failed to store ${request.url || request} in ${cacheName}.`;
-      console.warn(msg, err);
-      // ensure a response
-      response = new Response();
-    }
-    return response;
-  }),
-  saveToCache: async(function*(request, response, cacheName) {
-    const cache = yield caches.open(cacheName);
-    yield cache.put(request, response.clone());
-  }),
-};
+const SITE_CACHE = "site-v1";
 
 // Let's set up life cycle listeners ("install", "activate", "fetch")
 self.addEventListener("install", (ev) => {
-  ev.waitUntil(self.skipWaiting());
-  async.task(function*() {
-      yield SWTasks.deleteAllCaches();
-      yield SWTasks.cacheSite(siteFiles, SITE_CACHE);
-      console.info("Successfully completed install tasks...");
-    })
-    .catch(
-      err => console.error("Caching went horribly wrong", err)
-    );
+  // Fill me in!
+  ev.waitUntil(async.task(function*() {
+    const cache = yield caches.open(SITE_CACHE);
+    yield cache.addAll(siteFiles);
+    console.log("Done adding site files");
+  }));
 });
 
-self.addEventListener("activate", (ev) => {
-  console.info("Service Worker activated");
-  ev.waitUntil(self.clients.claim());
+self.addEventListener("activate", () => {
+  // fill me in
+  //delete old caches
+  async.task(function*() {
+    const keys = yield caches.keys();
+    yield keys
+      .filter(key => key !== SITE_CACHE && key.startsWith("site-"))
+      .map(key => caches.delete(key));
+  });
 });
 
 self.addEventListener("fetch", (ev) => {
-  console.info("Handling", ev.request.url, "going to network first...");
-  ev.respondWith(SWTasks.networkFirst(ev.request, SITE_CACHE));
+  // Fill me in!
+  ev.respondWith(async.task(function*() {
+    const response = yield caches.match(ev.request);
+    if (response) {
+      return response;
+    }
+    const netResponse = yield fetch(ev.request);
+    if (netResponse.status === 404) {
+      return new Response(`
+        <h1>Oh Noes!</h1>
+        <p>Back to <a href="/">index</a></p>
+        `, {
+        headers: {
+          "Content-Type": "text/html"
+        }
+      });
+    }
+    return netResponse;
+  }));
+});
+
+self.addEventListener("message", ({ data }) => {
+  // Fill me in!
+  console.log("SW Message", data);
+  switch (data.action) {
+    case "skipWaiting":
+      self.skipWaiting();
+      break;
+  }
 });
